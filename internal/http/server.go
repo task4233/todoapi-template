c8ccc5499cecafdca5d9e9ab4d222f1ffd14dbb8
task4233 <tks.m1205@gmail.com>
2021-02-13 05:06:24 +0900

    update directory structure for test

--- internal/http/server.go
+++ internal/http/server.go
@@ -1,7 +1,6 @@
 package http
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 
@@ -10,7 +9,7 @@ import (
 
 // Server is a struct for *http.Server
 type Server struct {
-	server *http.Server
+	Server *http.Server
 }
 
 // NewServer provides a pointer to Server instance
@@ -21,27 +20,9 @@ func NewServer(port int, d db.DB) *Server {
 	mux.Handle("/list", &listHandler{db: d})
 
 	return &Server{
-		server: &http.Server{
+		Server: &http.Server{
 			Addr:    fmt.Sprintf(":%d", port),
 			Handler: mux,
 		},
 	}
-}
-
-// Start starts server
-func (s *Server) Start() error {
-	if err := s.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		return fmt.Errorf("failed to server: %w", err)
-	}
-
-	return nil
-}
-
-// Stop stops server
-func (s *Server) Stop(ctx context.Context) error {
-	if err := s.server.Shutdown(ctx); err != nil {
-		return fmt.Errorf("failed to shutdown: %w", err)
-	}
-
-	return nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
 
 import (
-	"github.com/task4233/todoapi-template/internal/server"
+	"context"
+	"fmt"
+	ht "net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/task4233/todoapi-template/internal/db"
+	"github.com/task4233/todoapi-template/internal/http"
 )
 
+const addr = 8080
+
 func main() {
-	server.Run()
+	Run()
+}
+
+// Run runs server with context
+func Run() {
+	os.Exit(run(context.Background()))
+}
+
+func run(ctx context.Context) int {
+	termCh := make(chan os.Signal, 1)
+	signal.Notify(termCh, syscall.SIGTERM, syscall.SIGINT)
+
+	d := db.NewMemoryDB()
+	s := http.NewServer(addr, d)
+	errCh := make(chan error, 1)
+
+	go func() {
+		fmt.Printf("Listening on :%d", addr)
+		if err := s.Server.ListenAndServe(); err != nil && err != ht.ErrServerClosed {
+			errCh <- fmt.Errorf("failed to server: %w", err)
+		}
+		errCh <- nil
+	}()
+
+	select {
+	case <-termCh:
+		if err := s.Server.Shutdown(ctx); err != nil {
+			errCh <- fmt.Errorf("failed to shutdown: %w", err)
+		}
+		return 0
+	case <-errCh:
+		if err := s.Server.Shutdown(ctx); err != nil {
+			errCh <- fmt.Errorf("failed to shutdown: %w", err)
+		}
+		return 1
+	}
 }
